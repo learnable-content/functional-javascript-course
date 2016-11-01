eb2346773629c490ee23430302267456f5fd5c8b
spxds <mactechy@gmail.com>
2016-11-01 12:00:08 -0400

    Add files via upload

---
+++ 1.6.0.js
@@ -0,0 +1,8 @@
+document.getElementById("clicker").addEventListener("click", function(e) {
+  alert(`you triggered ${this.id}`);
+});
+
+const proveIt = function(e) {
+  alert(`you triggered ${this.id}`);
+}
+document.getElementById("clicker").addEventListener("click", proveIt);
---
+++ 1.6.1.js
@@ -0,0 +1,7 @@
+const snakify = text => text.replace(/millenials/ig, "Snake People");
+console.log(snakify("The Millenials are always up to something."));
+// The Snake People are always up to something.
+
+const hippify = text => text.replace(/baby boomers/ig, "Aging Hippies");
+console.log(hippify("The Baby Boomers just look the other way."));
+// The Aging Hippies just look the other way.
