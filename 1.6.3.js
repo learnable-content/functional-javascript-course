2b1b99cd5166b98849ab61eb5af9b5da09efa5f0
spxds <mactechy@gmail.com>
2016-11-01 12:00:24 -0400

    Add files via upload

---
+++ 1.6.3.js
@@ -0,0 +1,8 @@
+const attitude = (original, replacement) => source => source.replace(original, replacement);
+const snakify = attitude(/millenials/ig, "Snake People");
+const hippify = attitude(/baby boomers/ig, "Aging Hippies");
+
+console.log(snakify("The Millenials are always up to something."));
+// The Snake People are always up to something.
+console.log(hippify("The Baby Boomers just look the other way."));
+// The Aging Hippies just look the other way.
